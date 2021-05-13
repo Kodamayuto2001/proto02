703b5247bf48ca20283046707033dd07bafbb6f2
kodamayuto <yuto2001gyokugan@gmail.com>
2021-05-14 00:41:01 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="header">
+    <nav class="gnav">
+      <ul class="menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">Login</a></li>
+      </ul>
+    </nav>
+  </div>
+  
+  <img alt="Vue logo" src="./assets/logo.png" class="text-logo">
+  <TopPage msg=""/>
+</template>
+
+<script>
+import TopPage from './components/TopPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopPage
+  }
+}
+</script>
+
+<style>
+img.text-logo {
+  margin-top: 10px;
+}
+body {
+  padding-top: 90px; /* ヘッダーの後ろに要素が隠れないようにするため */
+  background-color: #FFF;
+}
+.header {
+  width: 100%; /* 幅いっぱいを指定 */
+  height: 50px; /* 高さを50pxに指定 */
+  background: #CCC; /* 背景色にグレーを指定 */
+  padding: 20px 50px; /* ヘッダーに上下左右それぞれ余白を指定 */
+  box-sizing: border-box; /* padding分を含んで幅を100%にするため */
+  position: fixed; /* ウィンドウを基準に画面に固定 */
+  top: 0; /* 上下の固定位置を上から0pxにする */
+  left: 0; /* 左右の固定位置を左から0pxにする */
+  display: flex; /* 中の要素を横並びにする */
+  align-items: center; /* 中の要素を上下中央に並べる */
+  background-color: #321E10;
+}
+.header .gnav .menu {
+  display: flex; /* 中の要素を横並びにする */
+}
+
+.header .gnav .menu li a {
+  color: #FFF;
+  list-style: none; /* リストの[・]を消す */
+}
+
+.header .gnav .menu li + li {
+    margin-left: 40px; /* メニューそれぞれに間隔をあけるため */
+}
+
+main {
+    height: 100vw; /* スクロールの演出を見れるようにmainに高さを指定 */
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* .header .gnav .menu {
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+}
+.header .gnav .menu ::after{
+  position: absolute;
+  bottom: -4px;
+  left: 0;
+  content: '';
+  width: 100%;
+  height: 2px;
+  background: #333;
+  transform: scale(0, 1);
+  transform-origin: left top;
+  transition: transform .3s;
+}
+.header .gnav .menu :hover::after {
+  transform: scale(1, 1);
+} */
+</style>
---
+++ src/components/TopPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="body">
+    <!-- <h1>{{ msg }}</h1> -->
+    <h1 class="main-text">
+      zoom盛り上げたい
+    </h1>
+    
+    <a href="http://localhost:8080/index.html" class="botton">今すぐつかう</a>
+    <!-- <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p> -->
+    <!-- <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3> -->
+    <!-- <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul> -->
+    <!-- <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-text {
+  color: #111032;
+}
+
+.botton {
+  position: relative;
+  display: inline-block;
+  padding: 0.25em 0.5em;
+  text-decoration: none;
+  color: #FFF;
+  /* background: #42b983; */
+  background: #57558B;
+  border-radius: 4px;/*角の丸み*/
+  box-shadow: inset 0 2px 0 rgba(255,255,255,0.2), inset 0 -2px 0 rgba(0, 0, 0, 0.05);
+  font-weight: bold;
+  height: 40px;
+  /* border: solid 0px #d27d00; */
+  text-align: center;
+  line-height: 40px;
+  font-size: 30px;
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all  0.3s ease;
+}
+.botton:hover {
+  
+  background-color: #8610A8;
+  padding: 20px;
+  font-size: 40px;
+}
+
+.btn-square-so-pop:active {
+  /*押したとき*/
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.30);
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
